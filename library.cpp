cf09af32a95a7ed863aa59bdaf0145a78192ce21
ahmet-umut <95590170+ahmet-umut@users.noreply.github.com>
2023-07-11 14:25:36 +0300

    add some explanations

--- library.cpp
+++ library.cpp
@@ -3,7 +3,7 @@
 
 SDL_Renderer *renderer;
 SDL_Rect _1,_2;
-dc (int x, int y, int r) {	//pi=4
+dc (int x, int y, int r) {	//draw filled circle
 	for (float a=0; a<=1; a+=.3) {
 		_1.x = x-r*(cos(a)+.015), _1.y = y-r*(sin(a)+.015), _1.w = 2*r*(cos(a)+.015), _1.h = 2*r*(sin(a)+.015);
 		_2.x = x-r*(sin(a)+.015), _2.y = y-r*(cos(a)+.015), _2.w = 2*r*(sin(a)+.015), _2.h = 2*r*(cos(a)+.015);
@@ -11,4 +11,3 @@ dc (int x, int y, int r) {	//pi=4
 		SDL_RenderFillRect(renderer,&_2);
 		}
 	}	
-
