bef362e86bb99a8217ee594d0a48ef34b689fe65
ahmet-umut <95590170+ahmet-umut@users.noreply.github.com>
2023-07-11 14:23:13 +0300

    Update back-stab-gammon.cpp

--- back-stab-gammon.cpp
+++ back-stab-gammon.cpp
@@ -13,26 +13,23 @@ using namespace std;
 scolor (color c) {SDL_SetRenderDrawColor(renderer,c.r,c.g,c.b,255);}	//sets the drawing color for the renderer
 #define sclex 999 /*screen length x*/
 #define scley 888 /*screen length y*/
-#define mumap multimap
 
 color colors[11] = {{255,255,255},{200,200,200},{222,222,222},{111,138,77},{0,0,0}};	//22	20	2	(10,11,02)2	0
-color &bgrcolor=colors[0], &trcolor=colors[1], &bgacolor=colors[2]
+color &bgrcolor=colors[0], &trcolor=colors[1], &bgacolor=colors[2]	//3 and 4 is for the players false and true
 ;//		background		timer			backgammon
 
-#define animac 3
+#define animac 3	/*animation const	for the animations to feel right*/
 class anima {public: int x1,x2,y1,y2,radiu, step,step0;	bool playe;
 	anima(int x1, int y1,int x2,int y2,int radiu,bool playe) : x1(x1), x2(x2), y1(y1), y2(y2), radiu(radiu), playe(playe), step(pow(pow(x1-x2,2)+pow(y1-y2,2),1./animac))	{step0=step;}
 	draw() {	scolor(bgrcolor);	dc(x2,y2,radiu);	scolor(colors[3+playe]);	dc(x2+(x1-x2)*step/step0, y2+(y1-y2)*step/step0,radiu);	drali(x1,y1,x2,y2);		return step--<0;	}	//retruns true when the animation ends
 	};
+stack<anima> animas;	//animations
 
-stack<int> si;	//intermedieate stack int
-stack<anima> sanima;
-stack<bool> s;	//intermediate stack
-stack<bool> table[25];	//table_0 is for the pop_back process
-bool ganima=true;
-int eates[2];	//eatens
 struct haml {int from,to; bool iescap=false;};	//iescap: is escape hamla
-stack<haml> hamls;
+stack<haml> hamls;	//for selection
+stack<bool> table[25];	//backgammon table
+int eates[2];	//eaten rocks of 2 players
+
 /*
 <--			
 |false	(gray)
@@ -43,7 +40,7 @@ TABLE VIEW
 <--		
 */
 
-mumap<int,int>keytr;	//key timer
+multimap<int,int>keytr;	//key timer
 
 template <class t> t& operator*(stack<t>&s)	{return s.top();}	//top()
 template <class t> bool operator<<(stack<t>&s2, stack<t>&s1)	{	if (s1.empty())	return false;		s2.push(*s1), s1.pop();	return true;	}	//aktarmak
@@ -63,7 +60,7 @@ isput(bool playe, int tablei, int chang = true) {	//can playe put his rock at ta
 		table[tablei].pop();	eates[!playe]++;	if (table[tablei].size() && *table[tablei] == !playe)
 			{	table[tablei].push(!playe);	eates[!playe]--;	return false;	}	//2 oponent rocks are on the top 
 
-		if (chang)	{ sanima.emplace(x1,y1,x2,y2, rocsc, !playe);	}
+		if (chang)	{ animas.emplace(x1,y1,x2,y2, rocsc, !playe);	}
 		else {	table[tablei].push(!playe);	eates[!playe]--;	}
 		return true;
 		;}
@@ -82,28 +79,28 @@ movfr (bool playe, int from, int n)
 movto (bool playe, int to, int n)
 	{	if (to-n*(1-2*playe) > 24 || to-n*(1-2*playe) < 1)	return false;	return movfrto(playe,to-n*(1-2*playe),to);	}
 
-mhaml (bool playe, haml hamla) {stack<bool>si;	int x1 = getx(hamla.from), y1;	//make hamla
-	if (hamla.iescap)	while(*table[hamla.from] != playe)	si << table[hamla.from];
-	y1 = gety(hamla.from);	isput(playe,hamla.to);	table[hamla.to]<<table[hamla.from]; sanima.emplace(x1,y1,getx(hamla.to),gety(hamla.to), rocsc, playe);
-	while (table[hamla.from]<<si);
+mhaml (bool playe, haml hamla) {stack<bool>sb;	int x1 = getx(hamla.from), y1;	//make hamla
+	if (hamla.iescap)	while(*table[hamla.from] != playe)	sb << table[hamla.from];
+	y1 = gety(hamla.from);	isput(playe,hamla.to);	table[hamla.to]<<table[hamla.from]; animas.emplace(x1,y1,getx(hamla.to),gety(hamla.to), rocsc, playe);
+	while (table[hamla.from]<<sb);
 	;}
 movfo (bool playe, int dice=0) {	//move forced
 	hamls = stack<haml>();	if (!dice)	dice = rauni%6+1;
 
-	bool endgame=true, toproc,look;		
+	bool endgame=true, toproc,look;	stack<bool>sb;
 	for (int i=1; i!=25; i++) {
-		if (table[i].size())	toproc=*table[i];	s = stack<bool>();	look=true;
-		while (s<<table[i])
-			if (*s==playe && (playe && i>6 || !playe && i<19))
+		if (table[i].size())	toproc=*table[i];	sb = stack<bool>();	look=true;
+		while (sb<<table[i])
+			if (*sb==playe && (playe && i>6 || !playe && i<19))
 				{	if (toproc!=playe && isput(playe, playe? 25-dice : dice, false) && look)	{	look=false;	hamls.push({i, playe? 25-dice : dice, true});	}	endgame=false;	}
-		while(table[i]<<s);
+		while(table[i]<<sb);
 		;}
 	stack<int> si;
 	if (endgame) {
 		for (int i = playe?6:24, j=6; i != (playe?0:18); i--, j--)	{	if (table[i].size() && (si.empty() || j>=dice))	si.push(i);	}	if (si.empty())	return false;
 		int hamli = rauni % si.size();	for (;hamli--;)	si.pop();
 		if (movfr(playe,*si,dice))	mhaml(playe,*hamls);	else	table[*si].pop();
-		sanima.emplace(1,1,99,99,1,1);
+		animas.emplace(1,1,99,99,1,1);
 		return true;
 		}
 
@@ -120,7 +117,7 @@ play (bool playe) {int remai=2, dice1,dice2, x1,y1, dice, tablei;	bool diceu[3] 
 	si = stack<int>();
 	while (eates[playe] && dices.size()) {	//finish all eaten rocks of that player if exists
 		if (isput(playe, tablei = playe ? 25-*dices : *dices)) {
-			dices.pop();	table[tablei].push(playe);	eates[playe]--;		sanima.emplace(x1,y1,getx(tablei),gety(tablei), rocsc,playe);
+			dices.pop();	table[tablei].push(playe);	eates[playe]--;		animas.emplace(x1,y1,getx(tablei),gety(tablei), rocsc,playe);
 			}
 		else	si<<dices;
 		}
@@ -133,19 +130,18 @@ play (bool playe) {int remai=2, dice1,dice2, x1,y1, dice, tablei;	bool diceu[3] 
 
 main (int argv, char** args) {int keboi;
 	;SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window *window = SDL_CreateWindow("SDL2 app", 100, 100, sclex,scley ,SDL_WINDOW_SHOWN);
+	SDL_Window *window = SDL_CreateWindow("Back-stab-gammon", 100, 100, sclex,scley ,SDL_WINDOW_SHOWN);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
 	SDL_RaiseWindow(window);
 
 	keytr.emplace(pair(SDL_SCANCODE_SPACE,-1));
 
-	for (int rocco=3; rocco--;)	{	char posit = rauni%2+12;		table[posit].push(true);	table[25-posit].push(false);	}
+	for (int rocco=5; rocco--;)	{	char posit = rauni%24+1;		table[posit].push(true);	table[25-posit].push(false);	}
 
 	int trrad=33, tlast=SDL_GetTicks();	//tir: timer radius
 	float t=0, dt=1/60.;	//t is the time passed since this line is executed.	dt is the resolution of our "update"s. We could do it faster by reducing dt, or making several updates in a single loop step. My screen is 60 FPS; should it be 144, I'd make dt 1/144 (seconds)
 
-	int hamco=rauni%2;	//determine the first player
-	bool playui=true;	//play until interrupt
+	int hamco = rauni%2;	//determine the first player
 
 	SDL_Event event;	//to not make the program seem like in a not responding state, and to get the keyboard input properly since the SDL_PollEvent fn. takes events such as keyboard events...
 	while(1)
@@ -170,36 +166,38 @@ main (int argv, char** args) {int keboi;
 			for (int a = eates[1]; a--;)	scolor(colors[4]), dc(sclex-rocsc,a*rocsc,rocsc);
 
 			int i=1;
+			stack<bool>sb;
 			for (;i<7; i++) {	//>up
-				s = stack<bool>();
-				while (table[i].size())	{	scolor(colors[3+*table[i]]);	dc(getx(i),gety(i),rocsc);	s<<table[i];	}
-				while (s.size())	table[i]<<s;
+				sb = stack<bool>();
+				while (table[i].size())	{	scolor(colors[3+*table[i]]);	dc(getx(i),gety(i),rocsc);	sb<<table[i];	}
+				while (sb.size())	table[i]<<sb;
 				;}
 			for (;i<13; i++) {	//<up
-				s = stack<bool>();
-				while (table[i].size())	{	scolor(colors[3+*table[i]]);	dc(getx(i),gety(i),rocsc);	s<<table[i];	}
-				while (s.size())	table[i]<<s;
+				sb = stack<bool>();
+				while (table[i].size())	{	scolor(colors[3+*table[i]]);	dc(getx(i),gety(i),rocsc);	sb<<table[i];	}
+				while (sb.size())	table[i]<<sb;
 				;}
 			for (;i<19; i++) {	//<down
-				s = stack<bool>();
-				while (table[i].size())	{	scolor(colors[3+*table[i]]);	dc(getx(i),gety(i),rocsc);	s<<table[i];	}
-				while (s.size())	table[i]<<s;
+				sb = stack<bool>();
+				while (table[i].size())	{	scolor(colors[3+*table[i]]);	dc(getx(i),gety(i),rocsc);	sb<<table[i];	}
+				while (sb.size())	table[i]<<sb;
 				;}
 			for (;i<25; i++) {	//>down
-				s = stack<bool>();
-				while (table[i].size())	{	scolor(colors[3+*table[i]]);	dc(getx(i),gety(i),rocsc);	s<<table[i];	}
-				while (s.size())	table[i]<<s;
+				sb = stack<bool>();
+				while (table[i].size())	{	scolor(colors[3+*table[i]]);	dc(getx(i),gety(i),rocsc);	sb<<table[i];	}
+				while (sb.size())	table[i]<<sb;
 				;}
 
 			//input handler
 			bool playe = hamco%2;
-			for (auto itera = keytr.begin(); itera != keytr.end(); itera++)	itera->second--;
-			if (SDL_GetKeyboardState(&keboi)[SDL_SCANCODE_SPACE] && keytr.find(SDL_SCANCODE_SPACE)->second<0)	{
+			for (auto itera = keytr.begin(); itera != keytr.end(); itera++)	itera->second;
+			if (SDL_GetKeyboardState(&keboi)[SDL_SCANCODE_SPACE])	keytr.find(SDL_SCANCODE_SPACE)->second = 22;
+			if (keytr.find(SDL_SCANCODE_SPACE)->second > 0 && animas.empty())	{
 				keytr.find(SDL_SCANCODE_SPACE)->second = 22;
 				if (!play(playe))break;
 				hamco++;}
 
-			if (!sanima.empty() && (*sanima).draw())	{sanima.pop();}
+			if (!animas.empty() && (*animas).draw())	{animas.pop();}
 
 
 			scolor(trcolor);
