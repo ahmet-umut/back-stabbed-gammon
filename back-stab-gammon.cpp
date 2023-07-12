7524eace745ee08f77910dba7c445d84e3ffeda5
ahmet-umut <95590170+ahmet-umut@users.noreply.github.com>
2023-07-12 15:30:12 +0300

    objectized most of the moves (hamla)

--- back-stab-gammon.cpp
+++ back-stab-gammon.cpp
@@ -18,7 +18,7 @@ color colors[11] = {{255,255,255},{200,200,200},{222,222,222},{111,138,77},{0,0,
 color &bgrcolor=colors[0], &trcolor=colors[1], &bgacolor=colors[2]	//3 and 4 is for the players false and true
 ;//		background		timer			backgammon
 
-#define animac 64	/*animation step dividor const*/
+#define animac 72	/*animation step dividor const*/
 class anima {public: int x1,x2,y1,y2,radiu;	float step,step0;	bool playe;
 	anima(int x1, int y1,int x2,int y2,int radiu,bool playe) : x1(x1), x2(x2), y1(y1), y2(y2), radiu(radiu), playe(playe), step(pow(pow(x1-x2,2)+pow(y1-y2,2),.4)/animac)	{step0=step;}
 	draw(float dt = 1/60.)
@@ -43,18 +43,20 @@ TABLE VIEW
 
 multimap<int,int>keytr;	//key timer
 
+template <class t> stack<t>& operator<<(stack<t>&s, t elemen)	{	s.push(elemen);	return s;	}	//so that we can push multiple elements inside a stack like this:	s<<e1<<e2<<...;
 template <class t> t& operator*(stack<t>&s)	{return s.top();}	//top()
 template <class t> bool operator<<(stack<t>&s2, stack<t>&s1)	{	if (s1.empty())	return false;		s2.push(*s1), s1.pop();	return true;	}	//aktarmak
 
 #define bgasc 64	/*backgammon scale*/
 #define rocsc 8	/*rock scale*/
 geteatex(bool playe)	{	return sclex-playe*rocsc;	}
-geteatey(bool playe)	{	return rocsc*eates[playe];	}
-getx(int tablei) {
+geteatey(bool playe, int incre=0)	{	return rocsc*(eates[playe]+incre);	}
+getx(int tablei)	//table
+	{
 	if (tablei<7) return sclex-tablei*bgasc;	else if (tablei<13)	return sclex-(2+tablei)*bgasc;	else if (tablei<19)	return sclex+(tablei-27)*bgasc;	else	return sclex+(tablei-25)*bgasc
 	;}
-gety(int tablei)	
-	{	if (tablei<13)	return 2*table[tablei].size()*rocsc;	else	return 4*bgasc-2*table[tablei].size()*rocsc;	}
+gety(int tablei, int incre=0)	//table indice	increment: how many imaginary rocks to assume under the normal place
+	{	if (tablei<13)	return (table[tablei].size() + incre) * rocsc*2;	else	return 4*bgasc - (table[tablei].size() + incre) * 2*rocsc;	}
 isput(bool playe, int tablei, int chang = true) {	//can playe put his rock at table[tablei]	chang: change. If set to false, does not make the opponent eaten
 	int x1 = getx(tablei), y1 = gety(tablei), x2 = geteatex(!playe), y2 = geteatey(!playe);
 	if (table[tablei].size() && *table[tablei] == !playe) {	//on top there is a opponent rock
@@ -68,15 +70,64 @@ isput(bool playe, int tablei, int chang = true) {	//can playe put his rock at ta
 	return true;
 	;}
 
-//ohaml = out hamla
+class hamla {public:	char hamlt;	int from,dice;	bool playe;
+	hamla(){}
+	hamla(bool playe, char hamlt, int from, int dice) : hamlt(hamlt), from(from), dice(dice), playe(playe) {}
+	hamla(bool playe, int dice) : hamlt('r'), dice(dice), playe(playe) {}
+
+	make()	//assumes that the hamla is makeable
+		{stack<bool>sb;		int x1 = getx(from), y1,	to;
+		cout<<playe;
+		switch (hamlt)
+			{
+			case 'e':	cout<<" e ";	while(*table[from] != playe)	sb << table[from];	//escape
+			case 'm':	//move
+				cout<<" m "<<from<<" " <<dice<<endl;
+				y1 = gety(from);
+				isput(playe, to = from + (playe?-dice:dice));	table[to]<<table[from];
+				animas.emplace(x1,y1,getx(to),gety(to), rocsc, playe);	while (table[from]<<sb);
+			break;
+			case 'r':	cout<<" r " << dice << endl;	isput(playe, to = playe ? 25-dice : dice);	table[to].push(playe);	eates[playe]--;		animas.emplace(geteatex(playe),geteatey(playe),getx(to),gety(to), rocsc, playe);	//recover (put eaten)
+			break;
+			case 'c':	cout<<" c "<<from<<endl;	table[from].pop();	//collect
+			}
+		}
+	show()
+		{stack<bool>sb;		int x1 = getx(from), y1,	to;
+		scolor(colors[3+playe]);
+		switch (hamlt)
+			{
+			case 'e':	while(*table[from] != playe)	sb << table[from];	//escape
+			case 'm':	//move
+				y1 = gety(from);
+				drali(x1,y1, getx(to),gety(to,1));	while (table[from]<<sb);
+			break;
+			case 'r':	drali(geteatex(playe),geteatey(playe,-1),getx(to),gety(to,1));	//recover (put eaten)
+			break;
+			case 'c':;
+			}
+		}
+	};
+
+class hamlse
+	{public:	bool playe;	hamla hamlas[4];	int hamlac=0;	//hamla sequence
+	hamlse (bool playe) : playe(playe)	{}
+	ahaml (int from, int dice, char hamlt='m') {	hamlas[hamlac] = hamla(playe,hamlt,from,dice);	hamlac++;	}
+	hamla& operator[] (int index)	{	return hamlas[index];	} 
+	char gett(int index)	{	return hamlas[index].hamlt;	}
+	char getdice(int index)	{	return hamlas[index].dice;	}
+	make()	{	for (int hamlai=0; hamlai!=hamlac; hamlai++)	hamlas[hamlai].make();	}
+	};
+
+stack<hamla>hamlas;
 movfrto (bool playe, int from, int to)	{
 	if (to==from || to<from != playe || table[from].empty() || *table[from] != playe)	return false;	//the source is not available or the direction is not right
 	if (!isput(playe,to,false))	return false;	//destination has at least 2 opponents on top	if only 1 opponent is on top, then that opponent is eaten
 	haml hamla;	hamla.from = from, hamla.to = to;	hamls.push(hamla);
 	return true;
 	;}
-movfr (bool playe, int from, int n)
-	{	if (from+n*(1-2*playe) > 24 || from+n*(1-2*playe) < 1)	return false;	return movfrto(playe,from,from+n*(1-2*playe));	}
+movfr (bool playe, int from, int n){
+	if (from+n*(1-2*playe) > 24 || from+n*(1-2*playe) < 1)	return false;	if (movfrto(playe,from,from+n*(1-2*playe)))	{	hamlas.emplace(playe,'m',from,n);	return true;	}	return false	;}
 movto (bool playe, int to, int n)
 	{	if (to-n*(1-2*playe) > 24 || to-n*(1-2*playe) < 1)	return false;	return movfrto(playe,to-n*(1-2*playe),to);	}
 
@@ -87,17 +138,19 @@ mhaml (bool playe, haml hamla) {stack<bool>sb;	int x1 = getx(hamla.from), y1;	//
 	;}
 movfo (bool playe, int dice=0) {	//move forced
 	hamls = stack<haml>();	if (!dice)	dice = rauni%6+1;
+	hamlas = stack<hamla>();
 
 	bool endgame=true, toproc,look;	stack<bool>sb;
 	for (int i=1; i!=25; i++) {
 		if (table[i].size())	toproc=*table[i];	sb = stack<bool>();	look=true;
 		while (sb<<table[i])
-			if (*sb==playe && (playe && i>6 || !playe && i<19))
-				{	if (toproc!=playe && isput(playe, playe? 25-dice : dice, false) && look)	{	look=false;	hamls.push({i, playe? 25-dice : dice, true});	}	endgame=false;	}
+			if (*sb==playe && (playe && i>6 || !playe && i<19))	//if there are directly uncollectable rocks of the player
+				{	if (toproc!=playe && isput(playe, playe? 25-dice : dice, false) && look)	{	look=false;	hamlas.emplace(playe,'e',i,dice);	}	endgame=false;	}
 		while(table[i]<<sb);
 		;}
 	stack<int> si;
-	if (endgame) {
+	if (endgame) {	//check later
+		cout<<"endgame"<<endl;
 		for (int i = playe?6:24, j=6; i != (playe?0:18); i--, j--)	{	if (table[i].size() && (si.empty() || j>=dice))	si.push(i);	}	if (si.empty())	return false;
 		int hamli = rauni % si.size();	for (;hamli--;)	si.pop();
 		if (movfr(playe,*si,dice))	mhaml(playe,*hamls);	else	table[*si].pop();
@@ -107,20 +160,20 @@ movfo (bool playe, int dice=0) {	//move forced
 
 	for (int i=1; i!=25; i++)	if (table[i].size() && *table[i]==playe && (playe && i>6 || !playe && i<19)) hamls = stack<haml>();
 
-	for (int i = 24; i; i--)	movfr(playe,i,dice);	if (hamls.empty())	return true;
-	int hamli = rauni % hamls.size();	for (;hamli--;)	hamls.pop();	mhaml(playe, *hamls);	//what if no hamlas??????
+	hamlas = stack<hamla>();
+	for (int i = 24; i; i--)	movfr(playe,i,dice);	if (hamlas.empty())	return true;
+	int hamli = rauni % hamlas.size();	for (;hamli--;)	hamlas.pop();	(*hamlas).make();	//what if no hamlas??????
 	return true;
 	;}
 play (bool playe) {int remai=2, dice1,dice2, x1,y1, dice, tablei;	bool diceu[3] = {0,0,0};	stack<int> dices,si;	//dice is used (diceu_0 is not used)	nanima: no animation
-	dice1 = rauni%6+1, dice2 = rauni%6+1;	if (dice1==dice2)	dices.push(dice1),dices.push(dice1),dices.push(dice1),dices.push(dice1);	else dices.push(dice1),dices.push(dice2);
+	dice1 = rauni%6+1, dice2 = rauni%6+1;	if (dice1==dice2)	dices<<dice1<<dice1<<dice1<<dice1;	else dices<<dice1<<dice2;
 	x1 = geteatex(playe), y1=geteatey(playe);	//copies for animation
 
 	si = stack<int>();
-	while (eates[playe] && dices.size()) {	//finish all eaten rocks of that player if exists
-		if (isput(playe, tablei = playe ? 25-*dices : *dices)) {
-			dices.pop();	table[tablei].push(playe);	eates[playe]--;		animas.emplace(x1,y1,getx(tablei),gety(tablei), rocsc,playe);
-			}
-		else	si<<dices;
+	while (eates[playe] && dices.size())
+		{	//finish all eaten rocks of that player if exists
+		if (isput(playe, tablei = playe ? 25-*dices : *dices, false))	{	hamla(playe,*dices).make();	dices.pop();	}
+		else si<<dices;
 		}
 	if (eates[playe])	return true;
 	while (dices<<si);
@@ -151,10 +204,8 @@ main (int argv, char** args) {int keboi;
 		if (SDL_GetTicks()-tlast > dt*1000) {pradt = (SDL_GetTicks()-tlast)/1000.;	tlast=SDL_GetTicks();
 			scolor(bgrcolor);	SDL_RenderClear(renderer);	//clears the renderer with background color
 
-			dci(444,444,333);
-
-			scolor(bgacolor);
 			//draw the backgammon  sticks	
+			scolor(bgacolor);
 			drali(sclex,bgasc/8,sclex-bgasc*14,bgasc/8), drali(sclex-bgasc*14,0,sclex-bgasc*14,bgasc*4), drali(sclex-bgasc*14,bgasc*4,sclex-bgasc,bgasc*4);	//table's indicator lines 
 			for (auto i=1; i<7; i++) {	
 				drali(sclex-i*bgasc,0,sclex-i*bgasc,bgasc), drali(sclex-(8+i)*bgasc,0,sclex-(8+i)*bgasc,bgasc);	//up><
@@ -191,16 +242,18 @@ main (int argv, char** args) {int keboi;
 				while (sb.size())	table[i]<<sb;
 				;}
 
-			//input handler
+			// input handler
+			for (auto itera = keytr.begin(); itera != keytr.end(); itera++)	itera->second--;
 			bool playe = hamco%2;
-			for (auto itera = keytr.begin(); itera != keytr.end(); itera++)	itera->second;
-			if (SDL_GetKeyboardState(&keboi)[SDL_SCANCODE_SPACE])	keytr.find(SDL_SCANCODE_SPACE)->second = 22;
-			if (keytr.find(SDL_SCANCODE_SPACE)->second > 0 && animas.empty())	{
+			// if (SDL_GetKeyboardState(&keboi)[SDL_SCANCODE_SPACE])	keytr.find(SDL_SCANCODE_SPACE)->second = 22;
+			if (keytr.find(SDL_SCANCODE_SPACE)->second < 0 && SDL_GetKeyboardState(&keboi)[SDL_SCANCODE_SPACE])
+				{
 				keytr.find(SDL_SCANCODE_SPACE)->second = 22;
-				if (!play(playe))break;
-				hamco++;}
+				if (!play(playe)) break;	//game ends
+				hamco++;
+				}
 
-			if (!animas.empty() && (*animas).draw(pradt))	{animas.pop();}
+			if (animas.size() && (*animas).draw(pradt))	{animas.pop();}
 
 
 			scolor(trcolor);
